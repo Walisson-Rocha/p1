e99cddb9b299a80a8a6b6c6f4911c55dc4c0b99a
Walisson <walissonoficial19@gmail.com>
2025-04-02 20:08:10 -0300

    tratamneto de dados

---
+++ Aula.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+diabetes_df = pd.read_csv("diabetes_clean.csv")
+print(diabetes_df.head())
+X = diabetes_df.drop("glucose", axis=1).values
+y = diabetes_df["glucose"].values
+print(type(X), type(y))
+X_bmi = X[:, 4]
+print(y.shape, X_bmi.shape)
+X_bmi = X_bmi.reshape(-1, 1)  # cria uma matriz 2D de uma única coluna.
+print(y.shape, X_bmi.shape)
+
+plt.scatter(X_bmi, y)
+plt.ylabel("Blood Glucose (mg/dl)")
+plt.xlabel("Body Mass Index")
+
+plt.show()
