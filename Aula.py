9b9730687536243976f33649bb5aaa7b0b6eda59
Walisson <walissonoficial19@gmail.com>
2025-04-02 20:14:38 -0300

    tratamento de dados 01

--- Aula.py
+++ Aula.py
@@ -11,8 +11,14 @@ print(y.shape, X_bmi.shape)
 X_bmi = X_bmi.reshape(-1, 1)  # cria uma matriz 2D de uma única coluna.
 print(y.shape, X_bmi.shape)
 
+from sklearn.linear_model import LinearRegression
+
+reg = LinearRegression()
+reg.fit(X_bmi, y)
+predictions = reg.predict(X_bmi)
+
 plt.scatter(X_bmi, y)
+plt.plot(X_bmi, predictions)
 plt.ylabel("Blood Glucose (mg/dl)")
 plt.xlabel("Body Mass Index")
-
 plt.show()
