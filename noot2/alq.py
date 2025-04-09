b88788211981f10ff6f3567d65c9a7ac2491484d
Walisson <walissonoficial19@gmail.com>
2025-04-09 20:46:01 -0300

    tartar 2

--- noot2/alq.py
+++ noot2/alq.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
 diabetes = pd.read_csv('./diabetes_clean.csv')
-diabetes.head()
+
+diabetes_df = diabetes[(diabetes['glucose'] > 0) & 
+                        (diabetes['insulin'] > 0) & 
+                        (diabetes['bmi'] > 30)]
+
+
+print(diabetes_df)
+
+x = diabetes_df.drop("glucose", axis=1).values
+y = diabetes_df["glucose"].values
+print(x.shape, y.shape)
